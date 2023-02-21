8651a677821df222045353aab7bbaf5a969fc3f0
bot <bendersilver@gmail.com>
2023-02-21 22:06:00 +0500

    dump

--- cmd/main.go
+++ cmd/main.go
@@ -10,13 +10,24 @@ import (
 // journalctl --user -n 10 -f -o cat
 
 func main() {
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "apply":
+			err := joura.SetPkgConfig()
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println("OK")
+			}
+		}
+	} else {
+		j, err := joura.New()
 
-	j, err := joura.New()
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		j.Start()
 	}
-	j.Start()
 
 }
