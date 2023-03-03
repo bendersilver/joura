f80745f686b89b17343fb93d65ab4931ab184fd0
bot <bendersilver@gmail.com>
2023-03-03 20:41:17 +0500

    pretty

--- build.bash
+++ build.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VER='0.1.1'
+VER='0.1.2'
 
 
 
--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/bendersilver/joura"
 )
 
-const Version = "0.1.1"
+const Version = "0.1.2"
 
 func main() {
 	var file string
